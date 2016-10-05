437d2c5a59f4c7dcb72eb3cd0a3bdea88dee05aa
r-vijay-r <503r.vijay@gmail.com>
2016-10-06 02:44:56 +0530

    update till 06/10/2016

--- database_sql/vijay.sql
+++ database_sql/vijay.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 25, 2016 at 09:03 PM
+-- Generation Time: Oct 05, 2016 at 11:12 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
 
@@ -14,6 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `vijay`
@@ -106,10 +107,69 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`pid`, `name`, `price`, `stock`) VALUES
-(1, 'Pen', 5, 29),
+(1, 'Pen', 5, 27),
 (2, 'Pencil', 3, 49),
 (3, 'Sharpner', 6, 20),
-(4, 'Paper', 1, 0);
+(4, 'Paper', 1, 0),
+(5, 'new', 10, 0),
+(6, 'new', 10, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `qid` int(4) NOT NULL,
+  `question` varchar(200) NOT NULL,
+  `op1` varchar(200) NOT NULL,
+  `op2` varchar(200) NOT NULL,
+  `op3` varchar(200) NOT NULL,
+  `op4` varchar(200) NOT NULL,
+  `answer` varchar(200) NOT NULL,
+  `mark` int(4) NOT NULL DEFAULT '0',
+  `level` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`qid`, `question`, `op1`, `op2`, `op3`, `op4`, `answer`, `mark`, `level`) VALUES
+(1, 'Which one of the following is in solid state?', 'Water', 'Petrol', 'Rock', 'Oxygen', 'Rock', 1, 'medium'),
+(2, 'Capital of India is : ', 'Kerala', 'Mumbai', 'Goa', 'Delhi', 'Delhi', 1, 'beginer'),
+(3, 'Lion is the king of ', 'House', 'Forest', 'Dogs', 'Hens', 'Forest', 1, 'medium');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `stdexamdet`
+--
+
+CREATE TABLE `stdexamdet` (
+  `id` int(4) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `gender` varchar(7) NOT NULL,
+  `age` int(4) NOT NULL,
+  `address` varchar(150) NOT NULL,
+  `mobile` bigint(11) NOT NULL,
+  `email` varchar(70) NOT NULL,
+  `uname` varchar(50) NOT NULL,
+  `pword` varchar(50) NOT NULL,
+  `beginer` int(5) DEFAULT NULL,
+  `medium` int(5) DEFAULT NULL,
+  `expert` int(5) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `stdexamdet`
+--
+
+INSERT INTO `stdexamdet` (`id`, `name`, `gender`, `age`, `address`, `mobile`, `email`, `uname`, `pword`, `beginer`, `medium`, `expert`) VALUES
+(0, 'admin', 'admin', 22, 'admin', 666, 'admin@admin.com', 'admin', 'admin', NULL, NULL, NULL),
+(1, 'vijay r', 'male', 21, 'soddisfare', 1234567890, 'vijay@soddisfare.com', 'vijay', 'vijay', NULL, NULL, NULL),
+(2, 'Madu Lakshmi', 'Female', 22, 'Soddisfare', 1234567890, 'ml@soddisfare.com', 'madu', 'madu', NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -140,6 +200,18 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`pid`);
 
 --
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`qid`);
+
+--
+-- Indexes for table `stdexamdet`
+--
+ALTER TABLE `stdexamdet`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -147,7 +219,7 @@ ALTER TABLE `products`
 -- AUTO_INCREMENT for table `details`
 --
 ALTER TABLE `details`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `mark`
 --
@@ -157,7 +229,17 @@ ALTER TABLE `mark`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `pid` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `pid` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `qid` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `stdexamdet`
+--
+ALTER TABLE `stdexamdet`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
